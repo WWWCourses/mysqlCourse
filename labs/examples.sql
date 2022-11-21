b004e14c0a1dcf279be707c506118ab1767f9ee8
Iva Popova <ivawebcourses@gmail.com>
2022-11-21 17:07:24 +0200

    Add files via upload

---
+++ labs/examples.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `artist` (
+  artist_id SMALLINT(5) NOT NULL DEFAULT 0,
+  fname CHAR(10) DEFAULT NULL,
+  lname VARCHAR(20) NOT NULL,
+  age TINYINT UNSIGNED
+);
+
+
+insert into artist (salary,price) values (99.456,99.456);
+
+'john\tsmith'
